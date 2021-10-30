f2de97bd1ad2818825382d7e76efa9e6ebeabf49
Ismail <bagayoko.ismail@gmail.com>
2021-10-29 22:20:55 -0400

    add premake

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+require "vscode"
+
+-- delete a file if the clean action is running
+newaction {
+   trigger = "clean" ,
+   description = "remove all bin and int files",
+   execute = function (  )
+      print("Removing  bin...")
+      os.rmdir("./bin")
+      print("Removing  interme...")
+      os.rmdir("./obj")
+      print("Removing  project files...")
+      os.rmdir("./.vscode")
+      os.remove("./**.code-workspace")
+      os.remove("./Makefile")
+      -- os.remove("**make)
+      print("Done")
+   end
+
+
+}
+workspace "Izi"
+   architecture "x64"
+   configurations { "Debug", "Release" }
+
+
+project "izi"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    location "../"
+    files {"**.h", "**.cpp"}
+
+    filter { "configurations:Debug" }
+       defines { "DEBUG" }
+       symbols "On"
+       runtime "Debug"
+ 
+    filter { "configurations:Release" }
+       defines { "NDEBUG" }
+       optimize "On"
+   -- filter { "system:linux", "action:gmake" }
+      -- buildoptions { "`wx-config --cxxflags`", "-ansi", "-pedantic" }
